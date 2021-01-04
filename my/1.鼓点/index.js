36dceb14fd9b4b1260e81a1fa5a923993cdfc3bb
xgg <fuzumin@outlook.com>
2021-01-04 22:00:47 +0800

    增加按键监听事件

--- my/1.鼓点/index.js
+++ my/1.鼓点/index.js
@@ -1,10 +1,54 @@
 (function() {
   "use strict";
+  const domArr = [];
+  const liArr = Array.from(document.querySelectorAll("ul li"));
+  const audioArr = Array.from(document.querySelectorAll(".audios audio"));
 
-  
+  liArr.forEach(item => {
+    const obj = {
+      text: item.textContent, // 按键名字
+      doms: item, // 对应按键DOM
+      audioDom: {}, // 对应音频DOM
+    };
+    for(const key of audioArr) {
+      if(key.dataset.keyvalue === item.textContent) {
+        obj.audioDom = key;
+        break;
+      }
+    }
+    domArr.push(obj)
+  });
+  console.log(domArr);
 
+  /**
+   * 根据按键获取元素本身
+   */
+  const getDom = function getDom(text) {
+    const arr = domArr.filter(item => {
+      if(text === item.text) {
+        return true;
+      }
+      return false;
+    });
+    if(arr.length) {
+      return [arr[0].doms]
+    } else {
+      return []
+    }
+  };
 
+  // 按下键
+  document.addEventListener("keydown", function(e) {
+    const arr = getDom(e.key.toLocaleUpperCase());
+    if(!arr.length) { return }
+    arr[0].classList.add("kd");
+  });
 
-
+  // 起来
+  document.addEventListener("keyup", function(e) {
+    const arr = getDom(e.key.toLocaleUpperCase());
+    if(!arr.length) { return }
+    arr[0].classList.remove("kd")
+  });
 
 })();
