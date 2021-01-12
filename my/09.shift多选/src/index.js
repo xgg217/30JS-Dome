0560eb795c27a2b25beefa3b56bfb70f7dc0bf68
fuzumin <fuzumin@outlook.com>
2021-01-12 18:12:20 +0800

    shift多选项目初始化2

---
+++ my/09.shift多选/src/index.js
@@ -0,0 +1,12 @@
+(function() {
+  "use strict";
+
+  const inboxDom = document.querySelector(".inbox");
+
+  document.addEventListener("keydown", function(e) {
+    if((e.target.nodeName !== "INPUT") && (e.key !== "Shift") ) { return; }
+    
+  })
+
+
+})();
