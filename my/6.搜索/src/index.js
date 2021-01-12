b8d6f7bded8821026c905f3d3645202812fed479
fuzumin <fuzumin@outlook.com>
2021-01-12 14:27:12 +0800

    完成搜索功能

--- my/6.搜索/src/index.js
+++ my/6.搜索/src/index.js
@@ -5,48 +5,75 @@
   const ulDom = document.querySelector("ul");
 
   /**
+   * 节流
+   * @param {*} func 
+   * @param {*} wait 
+   */
+  function throttle(func, wait) {
+    "use strict";
+    let timeout = 0;
+    let that = null;
+    return function(...args) {
+      that = this;
+      if(!!timeout) {
+        return
+      }
+      // 能走到这一步，证明上一次定时器已经执行完成
+      timeout = setTimeout(function() {
+        console.log(timeout);
+        timeout = 0;
+        func.apply(that, args);
+      }, wait);
+    }
+  }
+
+  /**
    * 根据搜索遍历内容
    */
   const seack = function seack(value) {
     const arr = tangArr.filter(item => {
-      if(item.contents.indexOf(value) !== -1) {
+      if(item.contents.includes(value) || item.title.includes(value) || item.author.includes(value) ) {
         return true;
       }
       return false;
     });
-    // console.log(arr);
     if(arr.length) {
-      setUlDom(arr);
+      setUlDom(arr, value);
+    } else {
+      ulDom.innerHTML = "";
     }
   };
 
   /**
    * 将搜索结构渲染成DOM
    */
-  const setUlDom = function setUlDom(arr) {
-    let str = ""
+  const setUlDom = function setUlDom(arr, value) {
+    let str = "";
+    const reg = new RegExp(value, 'gm');
     arr.forEach(item => {
-      str += `
+      let contenHTML = `
         <li>
           <p>${ item.contents }</p>
           <p>${ item.title }-${ item.author }</p>
         </li>
-      `
+      `;
+      const repHTML = contenHTML.replace(reg, a => {
+        return `<span>${ a }</span>`
+      });
+      str += repHTML;
     });
-
     ulDom.innerHTML = str;
   };
 
-
-
-  inputDom.addEventListener("input", function(e) {
+  const handleClick = function(e) {
     if(e.target.value) {
       seack(e.target.value)
     }
-    console.log(e.target.value);
-  });
+    // console.log(e.target.value);
+  };
 
+  const throttleFun = throttle(handleClick, 1000);
 
-
+  inputDom.addEventListener("input", throttleFun);
 
 })(tangArr);
